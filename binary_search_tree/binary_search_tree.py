9048cabd3ba0b464bfcc31121c67b70f8f2a4b32
Jonathan Ho <johnnyho82@gmail.com>
2020-01-29 18:43:50 -0500

    Write and test dft_print method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -49,7 +49,6 @@ class BinarySearchTree:
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-
     def in_order_print(self, node):
         self.left and self.left.in_order_print(self.left)
         print(self.value)
@@ -64,15 +63,26 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            node_to_pop = stack.storage.tail.value
+            print(node_to_pop.value)
+            stack.pop()
+            node_to_pop.right and stack.push(node_to_pop.right)
+            node_to_pop.left and stack.push(node_to_pop.left)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print In-order recursive DFT
+
     def pre_order_dft(self, node):
         pass
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+BinarySearchTree(3).dft_print(BinarySearchTree(3))
