c3aeb950cce013cca0d0f40c47ab9d62a9723afd
Jonathan Ho <johnnyho82@gmail.com>
2020-01-30 18:05:23 -0500

    Write and test all methods for generic Heap

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,55 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator=lambda x, y: x > y):
         self.storage = []
         self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(self.get_size() - 1)
 
     def delete(self):
-        pass
+        self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+        return_value = self.storage.pop()
+        self._sift_down(0)
+        return return_value
 
     def get_priority(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        parent_index = (index - 1) // 2
+
+        if index == 0:
+            return
+        else:
+            if self.comparator(self.storage[index], self.storage[parent_index]):
+                self.storage[index], self.storage[parent_index] \
+                    = self.storage[parent_index], self.storage[index]
+                self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        index_left_child = index * 2 + 1
+        index_right_child = index * 2 + 2
+
+        if index_left_child >= self.get_size():
+            return
+
+        elif self.comparator(self.storage[index], self.storage[index_left_child]) \
+                and self.comparator(self.storage[index], self.storage[index_right_child]):
+            return
+
+        else:
+            if index_right_child >= self.get_size():
+                switch_index = index_left_child
+            else:
+                switch_index = index_left_child \
+                    if self.comparator(
+                        self.storage[index_left_child], self.storage[index_right_child]) \
+                    else index_right_child
+
+            self.storage[index], self.storage[switch_index] \
+                = self.storage[switch_index], self.storage[index]
+            self._sift_down(switch_index)
