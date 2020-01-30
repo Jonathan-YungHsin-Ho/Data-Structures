67b7a4b1fe55e7c8c6a26eed124cd68bfe678bc1
Jonathan Ho <johnnyho82@gmail.com>
2020-01-30 17:08:57 -0500

    Stretch: Write and test Heap delete and sift_down methods

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -10,7 +10,14 @@ class Heap:
         self._bubble_up(self.get_size() - 1)
 
     def delete(self):
-        pass
+        if self.get_size() == 1:
+            return self.storage.pop()
+        else:
+            last_index = self.get_size() - 1
+            self.storage[0], self.storage[last_index] = self.storage[last_index], self.storage[0]
+            return_value = self.storage.pop()
+            self._sift_down(0)
+            return return_value
 
     def get_max(self):
         return self.storage[0]
@@ -28,18 +35,24 @@ class Heap:
                 self._bubble_up(parent_index)
 
     def _sift_down(self, index):
-        pass
+        # print('\n')
+        # print('Index', index)
+        # print('Value', self.storage[index])
+        index_left_child = index * 2 + 1
+        index_right_child = index * 2 + 2
 
-
-# test = Heap()
-
-# print(test._bubble_up(1))
-# print(test._bubble_up(2))
-# print(test._bubble_up(3))
-# print(test._bubble_up(4))
-# print(test._bubble_up(5))
-# print(test._bubble_up(6))
-# print(round(0.5))
-# print(round(1.5))
-# print(round(2.7))
-# print(round(3.5))
+        if index_left_child >= self.get_size():
+            # print('No children')
+            return
+        elif self.storage[index] >= self.storage[index_left_child] and self.storage[index] >= self.storage[index_right_child]:
+            # print('Larger than children')
+            return
+        else:
+            # print('Needs switch')
+            if index_right_child >= self.get_size():
+                switch_index = index_left_child
+            else:
+                switch_index = index_left_child if self.storage[
+                    index_left_child] >= self.storage[index_right_child] else index_right_child
+            self.storage[index], self.storage[switch_index] = self.storage[switch_index], self.storage[index]
+            self._sift_down(switch_index)
